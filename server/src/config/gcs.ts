d8160d48af68b8f0767945bef4c44655a1f15295
ifham baig <ifhambaig@ifham-ka-MacBook-Air.local>
2025-09-07 10:07:24 +0530

    commit

--- server/src/config/gcs.ts
+++ server/src/config/gcs.ts
@@ -1,16 +1,19 @@
 import { Storage, Bucket } from '@google-cloud/storage';
-import path from 'path';
-import { fileURLToPath } from 'url';
+// import path from 'path';
+// import { fileURLToPath } from 'url';
+import * as dotenv from 'dotenv'
+dotenv.config()
 
 // Get __dirname equivalent in ES Modules
-const __filename: string = fileURLToPath(import.meta.url);
-const __dirname: string = path.dirname(__filename);
+// const __filename: string = fileURLToPath(import.meta.url);
+// const __dirname: string = path.dirname(__filename);
 
 // Use relative path from current file to JSON key
-const keyPath: string = path.resolve(__dirname, '../../google_keys.json');
+const credentials = JSON.parse(process.env.GOOGLE_KEY!);
+// const keyPath: string = path.resolve(__dirname, '../../google_keys.json');
 
 const storage: Storage = new Storage({
-  keyFilename: keyPath,
+  keyFilename: credentials,
   projectId: 'deploy-4abde',
 });
 
